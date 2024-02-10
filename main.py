1104dffa37d0afc9b4ac0f08a33ab24d2513a87f
Jay <jay.nguyen@students.fhu.edu>
2024-02-09 20:13:29 -0600

    Update main.py
    attempt number 3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def anagram(word1, word2):
-    word1 = word1.strip()
-    word2 = word2.strip()
+    word1 = word1.replace(' ', '')
+    word2 = word2.replace(' ', '')
 
     word1 = word1.lower()
     word2 = word2.lower()
@@ -9,7 +9,9 @@ def anagram(word1, word2):
     word1 = sorted(word1)
     word2 = sorted(word2)
 
-    if word1 == word2:
+    if len(word1) == 0 or len(word2) == 0:
+        return False
+    elif word1 == word2:
         return True
     else:
         return False
