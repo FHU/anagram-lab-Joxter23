8952a58bcf5c7e4b90bfc4b46e161aacad4ca6c2
Jay <jay.nguyen@students.fhu.edu>
2024-02-09 19:55:48 -0600

    Update main.py
    first attempt

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(word1, word2):
+    word1 = word1.strip()
+    word2 = word2.strip()
+
+    word1 = word1.lower()
+    word2 = word2.lower()
+
+    word1 = sorted(word1)
+    word2 = sorted(word2)
+
+    if word1 == word2:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
-    #REMOVE PASS YOUR CODE GOES HERE
-    pass
+    word1 = input()
+    word2 = input()
+
+    print(anagram(word1, word2))
     
